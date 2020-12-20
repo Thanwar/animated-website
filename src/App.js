6092093963d64c3632e7f634b1ef1675ac2511ef
Thanwar <thawardas25@gmail.com>
2020-12-21 01:17:55 +0500

    completed

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+import backgroundi from './images/background.jpg';
+
+
+import Header from "./Components/Header";
+import Places from "./Components/Places";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '100vh',
+    backgroundImage: `url(${backgroundi})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  },
+}));
+
+
+function App() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+        <CssBaseline />
+        <Header />
+        <Places />
+        <div id="footer">
+          <h3>&copy; THANWAR 2021</h3>
+        </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/static/places.js
@@ -0,0 +1,23 @@
+import island1 from '../images/island2.jpg';
+import island2 from '../images/island3.jpg';
+
+
+
+const places = [
+  {
+    title: 'Maldives',
+    description:
+      "The Maldives are home to some of the world's most ravishing islands, but it's the sea, which truly makes these islands shine. Luminous aquamarine waters with a crystal clarity lap upon these dazzling white shores, which barely peek above the Indian Ocean.",
+    imageUrl: `${island1}`,
+    time: 1500,
+  },
+  {
+    title: 'Bora Bora',
+    description:
+      'Shaped like a giant sombrero, this lush volcanic island stars in countless South Pacific fantasies. The focal point and best asset of this tropical beauty is its ravishing lagoon in technicolor turquoise. Fish, turtles, sharks, and rays swim in the clear waters.',
+    imageUrl: `${island2}`,
+    time: 1500,
+  },
+];
+
+export default places;
